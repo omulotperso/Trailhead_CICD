96ec5b9a0725ead92d76fef6373681a9dfcba0f1
omulot <omulot.perso@gmail.com>
2025-10-26 19:32:30 +0100

    3eme envoi script

--- scripts/init-core-dev.sh
+++ scripts/init-core-dev.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 DEV_ALIAS="core_dev_$(whoami)"
+DEV_HUB_ALIAS="DevHub_Core"
 
-echo "🔧 Création de la Scratch Org : $DEV_ALIAS"
+echo "🔧 Création de la Scratch Org avec CLI sf : $DEV_ALIAS"
 
-sfdx force:org:create \
-  --definitionfile config/project-scratch-def.json \
-  --setalias $DEV_ALIAS \
-  --setdefaultusername \
-  --durationdays 30 \
-  --targetdevhubusername DevHub_Core
+sf org create scratch \
+  --definition-file config/project-scratch-def.json \
+  --alias $DEV_ALIAS \
+  --duration-days 7 \
+  --target-dev-hub $DEV_HUB_ALIAS \
+  --set-default
 
 echo "📤 Déploiement du code Core Model"
-sfdx force:source:push
+sf project deploy start
 
-echo "🧪 Lancement des tests"
-sfdx force:apex:test:run --resultformat human --wait 10
+echo "🧪 Lancement des tests Apex"
+sf apex run test --wait 10
 
 echo "✅ Scratch Org $DEV_ALIAS prête à l’usage"
