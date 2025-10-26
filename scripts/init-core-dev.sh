eaa8d4e0d08bf102a67a5433f0063fe6caf85dd3
omulot <omulot.perso@gmail.com>
2025-10-26 19:14:17 +0100

    script shell de creation de scratch org

---
+++ scripts/init-core-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEV_ALIAS="core_dev_$(whoami)"
+
+echo "🔧 Création de la Scratch Org : $DEV_ALIAS"
+
+sfdx force:org:create \
+  --definitionfile config/project-scratch-def.json \
+  --setalias $DEV_ALIAS \
+  --setdefaultusername \
+  --durationdays 7 \
+  --targetdevhubusername DevHub_Core
+
+echo "📤 Déploiement du code Core Model"
+sfdx force:source:push
+
+echo "🧪 Lancement des tests"
+sfdx force:apex:test:run --resultformat human --wait 10
+
+echo "✅ Scratch Org $DEV_ALIAS prête à l’usage"
